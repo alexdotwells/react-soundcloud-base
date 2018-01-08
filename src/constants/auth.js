a4859d36ad23b28b11231f749ea6931561cae443
alexdotwells <ajwells99@gmail.com>
2018-01-07 20:16:17 -0600

    updated auth.js

--- src/constants/auth.js
+++ src/constants/auth.js
@@ -1,2 +1,2 @@
-export const CLIENT_ID = '##_SoundCloudCliendID_##'; // SoundCloud Client ID
+export const CLIENT_ID = '##SoundCloudCliendID##'; // SoundCloud Client ID
 export const REDIRECT_URI = `${window.location.protocol}//${window.location.host}/callback`;
