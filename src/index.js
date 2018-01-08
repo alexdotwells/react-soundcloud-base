9176a0242e1297a0df49d971dee65a11fb2a7c9c
alexdotwells <ajwells99@gmail.com>
2018-01-07 18:02:39 -0600

    implemented Redux and added Container and Presenter Component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import configureStore from './stores/configureStore';
+import * as actions from './actions';
 import Stream from './components/Stream';
-
-const title = 'React SoundCloud Base';
 
 const tracks = [
   {
@@ -13,9 +14,13 @@ const tracks = [
   }
 ];
 
+const store = configureStore();
+store.dispatch(actions.setTracks(tracks));
 
 ReactDOM.render(
-  <Stream tracks={tracks} />,
+  <Provider store={store}>
+    <Stream />
+  </Provider>,
   document.getElementById('app')
 );
 
